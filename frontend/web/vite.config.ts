9785bce9c885f9294769c9e77090a255229ed592
Rei Sugiura <reibomaru@gmail.com>
2023-11-26 11:58:00 +0900

    chore: set vite config

--- frontend/web/vite.config.ts
+++ frontend/web/vite.config.ts
@@ -1,15 +1,16 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   server: {
-    port: 3000
+    port: 3000,
+    proxy: {
+      "/api": "http://localhost:8080",
+    },
   },
   resolve: {
-    alias: [
-      { find: '~/', replacement: `${__dirname}/src/` }
-    ]
+    alias: [{ find: "~/", replacement: `${__dirname}/src/` }],
   },
   plugins: [react()],
-})
+});
