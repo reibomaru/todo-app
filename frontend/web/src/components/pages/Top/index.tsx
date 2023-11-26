f39177b5dc6a47d5d832a5c561264e95c18fec0b
Rei Sugiura <reibomaru@gmail.com>
2023-11-26 11:56:54 +0900

    feat: impl temp top page

--- frontend/web/src/components/pages/Top/index.tsx
+++ frontend/web/src/components/pages/Top/index.tsx
@@ -3,7 +3,19 @@ import HeaderLayout from "~/components/organisms/HeaderLayout";
 const Top = () => {
   return (
     <>
-      <HeaderLayout>トップページ</HeaderLayout>
+      <HeaderLayout>
+        <h1>トップページ</h1>
+        <ul>
+          <li>
+            <a href="https://www.notion.so/enechain-d28edbd32f7946b3ac04f7d71f12b111">
+              ドキュメント
+            </a>
+          </li>
+          <li>
+            <a href="https://github.com/reibomaru/todo-app">リポジトリ</a>
+          </li>
+        </ul>
+      </HeaderLayout>
     </>
   );
 };
