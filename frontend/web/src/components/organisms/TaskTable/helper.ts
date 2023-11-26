b5e35f96b8c19687b1748116ec17db1e8418e878
Rei Sugiura <reibomaru@gmail.com>
2023-11-26 11:55:56 +0900

    feat: impl task table

---
+++ frontend/web/src/components/organisms/TaskTable/helper.ts
@@ -0,0 +1,34 @@
+import { Task } from "~/apis/backend/gen";
+
+export type Header = { key: keyof Task; value: string };
+
+export const headers: Header[] = [
+  {
+    key: "title",
+    value: "タイトル",
+  },
+  {
+    key: "due",
+    value: "期限",
+  },
+  {
+    key: "status",
+    value: "ステータス",
+  },
+  {
+    key: "pubclication_range",
+    value: "公開範囲",
+  },
+  {
+    key: "assignees",
+    value: "担当者",
+  },
+  {
+    key: "author",
+    value: "作成者",
+  },
+  {
+    key: "created_at",
+    value: "作成日",
+  },
+];
---
+++ frontend/web/src/components/organisms/TaskTable/index.tsx
@@ -0,0 +1,77 @@
+import {
+  Chip,
+  Paper,
+  Table,
+  TableBody,
+  TableCell,
+  TableContainer,
+  TableHead,
+  TableRow,
+} from "@mui/material";
+import { Task } from "~/apis/backend/gen";
+import { headers } from "./helper";
+import { memo } from "react";
+
+const taskEntryToView = (task: Task, key: keyof Task) => {
+  switch (key) {
+    case "title":
+    case "due":
+    case "created_at":
+    case "pubclication_range":
+      return <p>{task[key]}</p>;
+    case "status":
+      return <p>{task[key].name}</p>;
+    case "author":
+      return <p>{task[key].name}</p>;
+    case "assignees":
+      return task[key].map((assignee) => (
+        <Chip key={assignee.id} label={assignee.name} />
+      ));
+    default:
+      return <p>-</p>;
+  }
+};
+
+type Props = {
+  tasks: Task[];
+  currentSortKey: string;
+  onClickHeader: (key: keyof Task) => void;
+};
+
+const TaskTable = memo(({ tasks, currentSortKey, onClickHeader }: Props) => {
+  return (
+    <TableContainer component={Paper}>
+      <Table>
+        <TableHead>
+          <TableRow>
+            {headers.map(({ key, value }) => {
+              return (
+                <TableCell
+                  onClick={() => onClickHeader(key)}
+                  key={key}
+                  sx={{ cursor: "pointer" }}
+                >
+                  {value}
+                  {currentSortKey === key && "▼"}
+                </TableCell>
+              );
+            })}
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {tasks.map((task) => (
+            <TableRow key={task.id}>
+              {headers.map(({ key }) => {
+                return (
+                  <TableCell key={key}>{taskEntryToView(task, key)}</TableCell>
+                );
+              })}
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
+    </TableContainer>
+  );
+});
+
+export default TaskTable;
