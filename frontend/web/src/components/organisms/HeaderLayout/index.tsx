9beb0325ec701c4c7ffbbeca7a97851c6b8be774
Rei Sugiura <reibomaru@gmail.com>
2023-11-26 11:52:59 +0900

    feat: impl header layout component

--- frontend/web/src/components/organisms/HeaderLayout/index.tsx
+++ frontend/web/src/components/organisms/HeaderLayout/index.tsx
@@ -1,29 +1,36 @@
 import { ReactNode } from "react";
-import { AppBar, Button, IconButton, Toolbar, Typography } from "@mui/material";
-import MenuIcon from "@mui/icons-material/Menu";
+import { useNavigate } from "react-router-dom";
+import { AppBar, Button, Toolbar, Typography } from "@mui/material";
+import { useUser } from "~/hooks/UserContext/helper";
 
 type Props = {
   children: ReactNode;
 };
 
 const HeaderLayout = ({ children }: Props) => {
+  const user = useUser();
+  const navigate = useNavigate();
   return (
     <>
       <AppBar position="static">
         <Toolbar>
-          <IconButton
-            size="large"
-            edge="start"
-            color="inherit"
-            aria-label="menu"
-            sx={{ mr: 2 }}
-          >
-            <MenuIcon />
-          </IconButton>
           <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-            News
+            Todo APP
           </Typography>
-          <Button color="inherit">Login</Button>
+          {user ? (
+            <Button
+              onClick={() =>
+                navigate(`/${user.company.id}/tasks?assignee=${user.name}`)
+              }
+              color="inherit"
+            >
+              {user.name}
+            </Button>
+          ) : (
+            <Button onClick={() => navigate("/signin")} color="inherit">
+              サインイン
+            </Button>
+          )}
         </Toolbar>
       </AppBar>
       {children}
