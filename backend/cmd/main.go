cd2048e546210042fa60f145e5dd07553e032f96
Rei Sugiura <reibomaru@gmail.com>
2023-12-01 14:46:07 +0900

    refactor: move main.go

--- backend/cmd/main.go
+++ backend/cmd/main.go
@@ -1,11 +1,12 @@
-package apiserver
+package main
 
 import (
 	"fmt"
 	"os"
 
+	"github.com/joho/godotenv"
+
 	"github.com/gin-gonic/gin"
-	middleware "github.com/oapi-codegen/gin-middleware"
 	"github.com/reibomaru/todo-app/backend/internal/controller"
 	"github.com/reibomaru/todo-app/backend/internal/model"
 	"github.com/reibomaru/todo-app/backend/internal/service"
@@ -15,12 +16,12 @@ import (
 
 func main() {
 	r := gin.Default()
-	swagger, err := controller.GetSwagger()
+
+	err := godotenv.Load(".env")
 	if err != nil {
-		fmt.Fprintf(os.Stderr, "Error loading swagger spec\n: %s", err)
+		fmt.Fprintf(os.Stderr, "Error loading .env file")
 		os.Exit(1)
 	}
-	r.Use(middleware.OapiRequestValidator(swagger))
 
 	// Connect to DB
 	DataSourceName := os.Getenv("DATA_SOURCE_NAME")
@@ -34,8 +35,25 @@ func main() {
 		_ = sqlDB.Close()
 	}()
 
+	// r.Use(cors.New(cors.Config{
+	// 	AllowOrigins:     []string{"http://localhost:8081"},
+	// 	AllowMethods:     []string{"POST", "PUT", "PATCH"},
+	// 	ExposeHeaders:    []string{"Content-Length"},
+	// 	AllowCredentials: false,
+	// 	MaxAge: 12 * time.Hour,
+	// }))
+
+	// swagger, err := controller.GetSwagger()
+	// if err != nil {
+	// 	fmt.Fprintf(os.Stderr, "Error loading swagger spec\n: %s", err)
+	// 	os.Exit(1)
+	// }
+	// r.Use(middleware.OapiRequestValidator(swagger))
+
 	models := model.NewModels(db)
 	services := service.NewServices(models)
 	handlers := controller.NewHandlers(services)
 	controller.RegisterHandlers(r, handlers)
+
+	_ = r.Run(":8080")
 }
