696a7a07fe1480a46bbfdd43ea89f0e0bee4778c
Rei Sugiura <reibomaru@gmail.com>
2023-12-02 22:12:28 +0900

    chore: minor changed on config files

--- frontend/web/vite.config.ts
+++ frontend/web/vite.config.ts
@@ -4,10 +4,8 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   server: {
+    host: true,
     port: 3000,
-    proxy: {
-      "/api": "http://localhost:8080",
-    },
   },
   resolve: {
     alias: [{ find: "~/", replacement: `${__dirname}/src/` }],
